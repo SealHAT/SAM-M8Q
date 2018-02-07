292734c525b95abcde6ae66816a7a76b298e1a56
Ethan Slattery <eslatter@ucsc.edu>
2018-02-07 13:25:46 -0800

    added ubx.c

---
+++ ubx.c
@@ -0,0 +1,7 @@
+#include "ubx.h"
+
+void checksum(const uint8_t const * BUF, const uint8_t SIZE, uint8_t* ck_a, uint8_t* ck_b)
+{
+	ck_a = 0;
+	ck_b = 0;
+}
--- ubx.h
+++ ubx.h
@@ -16,12 +16,15 @@ typedef struct ubx_header {
  * Performs the UBX checksum on the buffer and returns the CK_A and CK_B values
  * by pointer to the calling function
  *
- * buf     - The buffer to run the checksum on, this should be the message buffer not
+ * BUF     - The buffer to run the checksum on, this should be the message buffer not
  *           includeing the sync characters (CLASS through the end of PAYLOAD)
+ * SIZE    - The size of the buffer to perform checksum on 
  * ck_a    - (out) pointer to the first byte of checksum 
  * ck_b    - (out) pointer Second byte of checksum 
  * RETURNS - true if successful, false if initialization fails
  */
-void checksum(const uint8_t const * buf, uint8_t* ck_a, uint8_t* ck_b);
+void checksum(const uint8_t const * BUF, const uint8_t SIZE, uint8_t* ck_a, uint8_t* ck_b);
+
+
 
 #endif	/* UBXTYPES_H_ */
