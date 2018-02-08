15eb007155c9bab5c3b1cafd3d05fcde3465226d
anthony <aakoutro@ucsc.edu>
2018-02-07 16:43:18 -0800

    add some ubx class headers

--- ubx.c
+++ ubx.c
@@ -1,5 +1,29 @@
 #include "ubx.h"
 
+/* Message Acknowledged, fixed-length, output, p143 */
+static const ubx_header ACK_ACK = {
+    .header     = UBXMSG_HEADER,
+    .msgclass   = UBX_ACK,
+    .id         = 0x01,
+    .length     = 0x0002
+};
+
+/* Message Not-Acknowledged, fixed-length, output, p143 */
+static const ubx_header ACK_NAK = {
+    .header     = UBXMSG_HEADER,
+    .msgclass   = UBX_ACK,
+    .id         = 0x00,
+    .length     = 0x0002
+};
+
+/* Antenna Control Settings, fixed-length, get/set, p154 */
+static const ubx_header CFG_ANT = {
+    .header     = UBXMSG_HEADER,
+    .msgclass   = UBX_CFG,
+    .id         = 0x13,
+    .length     = 0x0004
+};
+
 void checksum(const uint8_t const * BUF, const uint8_t SIZE, uint8_t* ck_a, uint8_t* ck_b)
 {
 	int i;		/* loop counter */
--- ubx.h
+++ ubx.h
@@ -1,14 +1,38 @@
 /* UBX variable type definitions go here */
+#include <stdint.h>
 
 #ifndef UBXTYPES_H_
 #define UBXTYPES_H_
 
-typedef struct ubx_header {
-	uint16_t header;
-	uint8_t  class;
-	uint8_t  id;
-	uint16_t length;
+#define UBXMSG_HEADER (0xB562)
+
+/* UBX message class identifiers */
+typedef enum
+{
+    UBX_NAV = 0x01, /* Navigation results messages */
+    UBX_RXM = 0x02, /* Receiver manager messages */
+    UBX_INF = 0x04, /* Information messages */
+    UBX_ACK = 0x05, /* ACK/NAK meassages */
+    UBX_CFG = 0x06, /* Configuration input messages */
+    UBX_UPD = 0x09, /* Firmware update messages */
+    UBX_MON = 0x0A, /* Monitoring messages */
+    UBX_AID = 0x0B, /* AssistNow aiding messages */
+    UBX_TIM = 0x0D, /* Timing messages */
+    UBX_ESF = 0x10, /* External sensor fusion messages */
+    UBX_MGA = 0x13, /* Multiple GNSS assistance messages */
+    UBX_LOG = 0x21, /* Logging messages */
+    UBX_SEC = 0x27, /* Security feature messages */
+    UBX_HNR = 0x28  /* High rate navigation results messages*/
+} UBX_CLASS;
+
+typedef struct ubx_header 
+{
+	uint16_t    header;
+	UBX_CLASS   msgclass;
+	uint8_t     id;
+	uint16_t    length;
 } ubx_header;
+
 
 /**
  * checksum
@@ -26,5 +50,16 @@ typedef struct ubx_header {
 void checksum(const uint8_t const * BUF, const uint8_t SIZE, uint8_t* ck_a, uint8_t* ck_b);
 
 
+/**
+ * getclass
+ *
+ * Parses a UBX message to determine the UBX message class.
+ *
+ * BUF     - The buffer to run the checksum on, this should be the message buffer not
+ *           includeing the sync characters (CLASS through the end of PAYLOAD)
+ * SIZE    - The size of the buffer to perform checksum on 
+ * RETURNS - The enumerated class group of the ubx message
+ */
+UBX_CLASS getclass(const uint8_t const * BUF, const uint8_t SIZE);
 
 #endif	/* UBXTYPES_H_ */
