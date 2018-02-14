f683880d6158fdbdba626fe55e73b602b8effe3d
anthony <aakoutro@ucsc.edu>
2018-02-14 15:14:00 -0800

    add profile enum and header fixes

--- gps.h
+++ gps.h
@@ -5,61 +5,85 @@
  * Due Date: 14 Feb 2018   <3
  */
 
-#ifndef UBXTYPES_H_
-#define UBXTYPES_H_
+#ifndef GPSINTERFACE_H_
+#define GPSINTERFACE_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <hpl_spi_m_sync.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- *	This type represents the UTC time data as given by the GPS device
+ * utc_time_t enum
+ *
+ * This type represents the UTC time data as given by the GPS device
  */
 typedef struct utc_time_t {
-    uint8_t        year;             /* 0 .. 255 (1980 == 0)         */
-    uint8_t        month;            /* 1 .. 12                      */
-    uint8_t        day;              /* 1 .. 31                      */
-    uint8_t        hour;             /* 0 .. 23                      */
-    uint8_t        minute;           /* 0 .. 59                      */
-    uint8_t        second;           /* 0 .. 60                      */ 
-    uint16_t       millis;           /* 0 .. 999                     */
+    uint8_t        year;             /**< 0 .. 257 (1980 == 0)         */
+    uint8_t        month;            /**< 1 .. 12                      */
+    uint8_t        day;              /**< 1 .. 31                      */
+    uint8_t        hour;             /**< 0 .. 23                      */
+    uint8_t        minute;           /**< 0 .. 59                      */
+    uint8_t        second;           /**< 0 .. 60                      */ 
+    uint16_t       millis;           /**< 0 .. 999                     */
 } utc_time_t;
 
 /**
+ * utc_time_t enum
+ *
  * This type represents A location message, parsed from the NMEA data
  * Received from the gps module
  */
 typedef struct location_t {
-    utc_time_t     time;             /* UTC date/time                */
-    uint16_t       mask;             /*                              */
-    uint8_t        correction;       /* GPS/UTC offset               */
-    uint8_t        type;             /* fix type                     */
-    int32_t        latitude;         /* (WGS84) degrees, 1e7         */
-    int32_t        longitude;        /* (WGS84) degrees, 1e7         */
-    int32_t        altitude;         /* (MSL) m, 1e3                 */
-    int32_t        separation;       /* (WGS84) = (MSL) + separation */
-    uint32_t       speed;            /* m/s, 1e3                     */
-    uint32_t       course;           /* degrees, 1e5                 */
-    int32_t        climb;            /* m/s, 1e3                     */
-    uint32_t       ehpe;             /* m, 1e3                       */
-    uint32_t       evpe;             /* m, 1e3                       */
-    uint8_t        quality;          /* fix quality                  */
-    uint8_t        numsv;            /* fix numsv                    */
-    uint16_t       pdop;             /* 1e2                          */
-    uint16_t       hdop;             /* 1e2                          */
-    uint16_t       vdop;             /* 1e2                          */
-    uint32_t       ticks;            /* system tick                  */
+    utc_time_t     time;             /**< UTC date/time                */
+    uint16_t       mask;             /**<                              */
+    uint8_t        correction;       /**< GPS/UTC offset               */
+    uint8_t        type;             /**< fix type                     */
+    int32_t        latitude;         /**< (WGS84) degrees, 1e7         */
+    int32_t        longitude;        /**< (WGS84) degrees, 1e7         */
+    int32_t        altitude;         /**< (MSL) m, 1e3                 */
+    int32_t        separation;       /**< (WGS84) = (MSL) + separation */
+    uint32_t       speed;            /**< m/s, 1e3                     */
+    uint32_t       course;           /**< degrees, 1e5                 */
+    int32_t        climb;            /**< m/s, 1e3                     */
+    uint32_t       ehpe;             /**< m, 1e3                       */
+    uint32_t       evpe;             /**< m, 1e3                       */
+    uint8_t        quality;          /**< fix quality                  */
+    uint8_t        numsv;            /**< fix numsv                    */
+    uint16_t       pdop;             /**< 1e2                          */
+    uint16_t       hdop;             /**< 1e2                          */
+    uint16_t       vdop;             /**< 1e2                          */
+    uint32_t       ticks;            /**< system tick                  */
 } location_t;
+
+/**
+ * GPS_PROFILE enum
+ *
+ * Each represents a predefined configuration scheme for the CAM8 
+ * gps module, implemenation left to GNSS device header
+ */
+typedef enum
+{
+    /* TODO: Verify accuracy/current of schemes */
+    GPS_PSMOO1H,    /**< 1hr rate, 7cm accuracy, 246 uA avg current    */
+    GPS_PSMOO30S    /**< 30s rate, 7cm accuracy, 26 mA avg current     */
+    /* TODO: Add profiles as they are tested */
+} GPS_PROFILE;
 
 /**
  * gps_init
  *
- * Initializes and starts the GPS module with the default sampling and
- * messaging rates
- *
+ * Initializes and starts the GPS module with the default sampling 
+ * and messaging rates
+ * 
+ * @param spi device descriptor from AtmelStart configuration
  * @return true if successful, false if initialization fails
  */
-bool gps_init();
+bool gps_init(struct spi_m_sync_descriptor *spi);
 
 /**
  * gps_getfix
@@ -67,10 +91,10 @@ bool gps_init();
  * Polls the GPS module for a single fix and the minimum recommended 
  * positional data
  *
- * @param fix reference to a gps fix data structure
+ * @param fix reference to a gps location structure
  * @return 0 if successful, integer status code otherwise
  */
-uint8_t gps_getfix(GpsData *fix );
+uint8_t gps_getfix(location_t *fix);
 
 /**
  * gps_gettime
@@ -80,7 +104,7 @@ uint8_t gps_getfix(GpsData *fix );
  * @param time reference to a time/date structure
  * @return 0 if successful, integer status code otherwise
  */
-uint8_t gps_gettime(Time *time);
+uint8_t gps_gettime(utc_time_t *time);
 
 /**
  * gps_setrate
@@ -104,8 +128,8 @@ bool gps_sleep();
 /**
  * gps_wake
  *
- * Enable the GPS module by waking it from low-power (sleep) mode, it will 
- * resume sampling according to last configuration
+ * Enable the GPS module by waking it from low-power (sleep) mode, 
+ * it will resume sampling according to last configuration
  *
  * @return true if successful, false if device fails to sleep
  */
@@ -114,15 +138,16 @@ bool gps_sleep();
 /**
  * gps_setprofile
  *
- * Configure the GPS module with a predefined sampling/power scheme
+ * Configure the GPS module with a predefined sampling/power 
+ * scheme
  *
  * @param profile preconfigured sampling profile
  * @returns true if successful, false if profile not set
  */
-bool gps_setprofile(const GpsProfile profile);
+bool gps_setprofile(const GPS_PROFILE profile);
   
 #ifdef __cplusplus
 }
 #endif
 
-#endif	/* UBXTYPES_H_ */
+#endif	/* GPSINTERFACE_H_ */
