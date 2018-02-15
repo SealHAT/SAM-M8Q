f4b314826bb3d04093f655ba194ca565a64904b1
Ethan Slattery <eslatter@ucsc.edu>
2018-02-15 12:31:47 -0800

    cleared out old files and added gps.c

---
+++ gps.c
@@ -0,0 +1,36 @@
+#include "gps.h"
+
+bool gps_init(struct spi_m_sync_descriptor *spi) 
+{
+
+}
+
+uint8_t gps_getfix(location_t *fix)
+{
+
+}
+
+uint8_t gps_gettime(utc_time_t *time)
+{
+
+}
+
+bool gps_setrate(const uint32_t period)
+{
+
+}
+
+bool gps_sleep()
+{
+
+}
+
+bool gps_sleep()
+{
+
+}
+
+bool gps_setprofile(const GPS_PROFILE profile)
+{
+
+}
